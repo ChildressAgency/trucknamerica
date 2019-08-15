dbedbb2fb1f195965d837e16a2535083b5df7203
childressdev <jcampbell@childressagency.com>
2019-08-15 16:33:52 -0400

    added

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -3,5 +3,31 @@
 */
 
 jQuery(document).ready(function($){
-
+  $('.partners').slick({
+    dots: false,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
 });
