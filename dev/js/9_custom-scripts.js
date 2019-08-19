ed70ae710867d46a13a3d4c4cd6ea77103cb65d3
childressdev <jcampbell@childressagency.com>
2019-08-19 15:50:32 -0400

    home demo styled

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -6,9 +6,12 @@ jQuery(document).ready(function($){
   $('.partners').slick({
     dots: false,
     speed: 300,
-    slidesToShow: 6,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
+    centerMade:true,
+    prevArrow: '<button type="button" class="partner-prev"><i class="fas fa-angle-right"></i></button>',
+    nextArrow: '<button type="button" class="partner-next"><i class="fas fa-angle-left"></i></button>',
     responsive: [
       {
         breakpoint: 991,
