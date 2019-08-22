2ad13f780e15c754c32276c9ecf2450c33ab2693
childressdev <jcampbell@childressagency.com>
2019-08-22 18:10:06 -0400

    added normal carousel heights

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -51,4 +51,35 @@ jQuery(document).ready(function($){
     $oldYear.removeClass('active');
     $($newYear).addClass('active');
   }
+
+  $('#hero-carousel.carousel-heights .carousel-inner .carousel-item').carouselHeights();
 });
+
+$.fn.carouselHeights = function () {
+  var items = $(this), //grab all slides
+    heights = [], //create empty array to store height values
+    tallest; //create variable to make note of the tallest slide
+
+  var normalizeHeights = function () {
+    items.each(function () { //add heights to array
+      heights.push($(this).height());
+    });
+    tallest = Math.max.apply(null, heights); //cache largest value
+    items.each(function () {
+      $(this).css('min-height', tallest + 'px');
+    });
+  };
+
+  normalizeHeights();
+
+  $(window).on('resize orientationchange', function () {
+    //reset vars
+    tallest = 0;
+    heights.length = 0;
+
+    items.each(function () {
+      $(this).css('min-height', '0'); //reset min-height
+    });
+    normalizeHeights(); //run it again 
+  });
+};
