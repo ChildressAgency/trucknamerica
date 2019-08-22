410b59f9dc2ad65393b6b5be3130b25c81c28b0a
childressdev <jcampbell@childressagency.com>
2019-08-22 18:12:35 -0400

    fixed missing option

--- wp-theme-files/partials/banner.php
+++ wp-theme-files/partials/banner.php
@@ -1,7 +1,7 @@
 <?php 
   $banner = get_field('site_banner', 'option');
   if($banner): ?>
-    <?php if(trucknamerica_can_display_hero_slide(get_field('banner_start_date'), get_field('banner_end_date'))): ?>
+    <?php if(trucknamerica_can_display_hero_slide(get_field('banner_start_date', 'option'), get_field('banner_end_date', 'option'))): ?>
       <div class="banner">
         <img src="<?php echo esc_url($banner['url']); ?>" class="img-fluid d-block mx-auto" alt="<?php echo esc_attr($banner['alt']); ?>" />
       </div>
