8f56ba835c27ab68be42f21f7ad948d5950e16eb
childressdev <jcampbell@childressagency.com>
2019-08-22 10:50:07 -0400

    added

---
+++ wp-theme-files/trucknamerica-core-functionality/trucknamerica-core-functionality.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name: trucknamerica.com Core Functionality
+ * Description: This contains all your site's core functionality so that it is theme independent. <strong>It should always be activated.</strong>
+ * Author: The Childress Agency
+ * Author URI: https://childressagency.com
+ * Version: 1.0
+ * Text Domain: maxvelocity
+ */
+if(!defined('ABSPATH')){ exit; }
+
+define('TRUCKNAMERICA_PLUGIN_DIR', dirname(__FILE__));
+define('TRUCKNAMERICA_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+/**
+ * Load ACF if not already loaded
+ */
+if(!class_exists('acf')){
+  require_once TRUCKNAMERICA_PLUGIN_DIR . '/vendors/advanced-custom-fields-pro/acf.php';
+  add_filter('acf/settings/path', 'trucknamerica_acf_settings_path');
+  add_filter('acf/settings/dir', 'trucknamerica_acf_settings_dir');
+}
+
+function trucknamerica_acf_settings_path($path){
+  $path = plugin_dir_path(__FILE__) . 'vendors/advanced-custom-fields-pro/';
+  return $path;
+}
+
+function trucknamerica_acf_settings_dir($dir){
+  $dir = plugin_dir_url(__FILE__) . 'vendors/advanced-custom-fields-pro/';
+  return $dir;
+}
+
+add_action('plugins_loaded', 'trucknamerica_load_textdomain');
+function trucknamerica_load_textdomain(){
+  load_plugin_textdomain('trucknamerica', false, basename(TRUCKNAMERICA_PLUGIN_DIR) . '/languages');
+}
