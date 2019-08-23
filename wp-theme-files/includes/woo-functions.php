27930a909b6f514fca728cd1e1fbde3171331552
childressdev <jcampbell@childressagency.com>
2019-08-23 12:13:40 -0400

    added product loop cat layout

--- wp-theme-files/includes/woo-functions.php
+++ wp-theme-files/includes/woo-functions.php
@@ -1,4 +1,21 @@
 <?php
+if(!defined('ABSPATH')){ exit; }
+
+remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
+
+add_action('woocommerce_before_main_content', 'trucknamerica_wrapper_start', 10);
+function trucknamerica_wrapper_start(){
+  echo '<main id="main">
+          <div class="container">';
+}
+
+add_action('woocommerce_after_main_content', 'trucknamerica_wrapper_end', 10);
+function trucknamerica_wrapper_end(){
+  echo '</div></main>';
+}
+
 //header cart
 add_action('trucknamerica_show_cart_link', 'trucknamerica_cart_link');
 function trucknamerica_cart_link(){
@@ -19,10 +36,49 @@ function trucknamerica_update_header_cart_count($fragments){
   return $fragments;
 }
 
-//call for price
+/**
+ * Call for Price
+ */
 add_filter('woocommerce_empty_price_html', 'trucknamerica_empty_price_html');
 add_filter('woocommerce_variable_empty_price_html', 'trucknamerica_empty_price_html');
 add_filter('woocommerce_variation_empty_price_html', 'trucknamerica_empty_price_html');
 function trucknamerica_empty_price_html(){
   return 'Call for price';
 }
+
+/**
+ * product category thumbnail
+ * @see content-product_cat.php
+ */
+remove_action('woocommerce_before_subcategory', 'woocommerce_template_loop_category_link_open', 10);
+remove_action('woocommerce_before_subcategory_title', 'woocommerce_subcategory_thumbnail', 10);
+remove_action('woocommerce_shop_loop_subcategory_title', 'woocommerce_template_loop_category_title', 10);
+remove_action('woocommerce_after_subcategory', 'woocommerce_template_loop_category_link_close', 10);
+
+add_action('woocommerce_before_subcategory', 'trucknamerica_template_loop_category_link_open', 10);
+function trucknamerica_template_loop_category_link_open($category){
+  echo '<div class="img-link">';
+}
+
+add_action('woocommerce_before_subcategory_title', 'trucknamerica_subcategory_thumbnail', 10);
+function trucknamerica_subcategory_thumbnail($category){
+  $thumbnail_id = get_term_meta($category->term_id, 'thumbnail_id', true);
+  $image_url = wp_get_attachment_url($thumbnail_id);
+
+  echo '<div class="img-link-inner" style="background-image:url(' . esc_url($image_url) . ');"></div>';
+}
+
+add_action('woocommerce_shop_loop_subcategory_title', 'trucknamerica_template_loop_category_title', 10);
+function trucknamerica_template_loop_category_title($category){
+  echo '<a href="' . esc_url(get_term_link($category, 'product_cat')) . '">';
+    echo '<span class="left-bottom">' . $category->name . '</span>';
+  echo '</a>';
+}
+
+add_action('woocommerce_after_subcategory', 'trucknamerica_template_loop_category_link_close', 10);
+function trucknamerica_template_loop_category_link_close($category){
+  echo '<div class="light-overlay"></div>';
+  echo '</div>';
+}
+
+remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10);
