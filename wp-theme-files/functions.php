b09ef11fa90b5195024b23a3d684c362517b7a28
childressdev <jcampbell@childressagency.com>
2019-09-09 17:54:14 -0400

    commented out footer template display

--- wp-theme-files/functions.php
+++ wp-theme-files/functions.php
@@ -1,10 +1,11 @@
 <?php
+/*
 add_action('wp_footer', 'show_template');
 function show_template() {
 	global $template;
 	print_r($template);
 }
-
+*/
 add_action('wp_enqueue_scripts', 'jquery_cdn');
 function jquery_cdn(){
   if(!is_admin()){
